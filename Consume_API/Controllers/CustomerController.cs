7a420663b937920f67055e463f8b9c9dc89e9392
Vishal-Agarwal-Git <vishalagar86@gmail.com>
2026-04-29 19:25:53 +0530

    Add project files.

---
+++ Consume_API/Controllers/CustomerController.cs
@@ -0,0 +1,41 @@
+﻿using Consume_API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+namespace Consume_API.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private string localURL = "https://localhost:7192/swagger/index.html";
+        public IActionResult Index()
+        {
+            List<Customer> data = new List<Customer>();
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(localURL);
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    HttpResponseMessage response = client.GetAsync("/Customer/GetAllCustomers").Result;
+                    client.Dispose();
+                    if(response.IsSuccessStatusCode)
+                    {
+                        string stringData = response.Content.ReadAsStringAsync().Result;
+                        data = JsonConvert.DeserializeObject<List<Customer>>(stringData);
+                    }
+                    else
+                    {
+                        TempData["error"] = $"{response.ReasonPhrase}";
+                    }
+                }
+            }
+            catch(Exception Ex)
+            {
+                TempData["exception"] = Ex.Message;
+            }
+            return View(data);
+        }
+    }
+}
---
+++ Consume_API/Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Consume_API.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string IsActive { get; set; }
+    }
+}
