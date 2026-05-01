478bfdd421a9f88dbf004b2aa7ee4463fba6dc07
Vishal-Agarwal-Git <vishalagar86@gmail.com>
2026-05-01 07:51:11 +0530

    Customer Model Edited

--- Consume_API/Models/Customer.cs
+++ Consume_API/Models/Customer.cs
@@ -6,8 +6,12 @@ namespace Consume_API.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required(ErrorMessage = "Please Enter Name")]
+        [StringLength(20, MinimumLength = 4, ErrorMessage = "Must be atlest 4 character long.")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Please Select One")]
         public string Gender { get; set; }
+        [Display(Name = "Active")]
         public string IsActive { get; set; }
     }
 }
